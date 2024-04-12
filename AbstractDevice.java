e6efac9a9d416416d0df4d5688802fc44a18b904
Артур <s862008@yandex.ru>
2024-04-12 13:43:16 +0300

    Update AbstractDevice.java

--- AbstractDevice.java
+++ AbstractDevice.java
@@ -34,8 +34,8 @@ public static final String[] CONTROL_CHARACTERS = {
 
     public abstract void Processing(String s);
 
-    public AbstractDevice() {
-
+    public AbstractDevice(Communicator c, Connection connection, int idDevice) {
+       // параметры коммуникации с прибором
     }
   
     protected void mSaveResult(String examid, String analogy, String result, String flag) {
